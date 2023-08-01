04b6f6573887dc81db822b2d3a3ad90f170033c8
Rh4ps0dy <bartosz.giera1219@gmail.com>
2023-08-01 23:26:45 +0200

    Added Problem no.2 solution
    - no.2 using while loop iterating through all terms <= 4M
    - no.1 using sum of arithmetic sequences

--- EulerProject/Program.cs
+++ EulerProject/Program.cs
@@ -9,6 +9,9 @@ class ProgramRunner
         {
             { 1, () => P1.Run() },
             { 2, () => P2.Run() },
+            { 3, () => P3.Run() },
+            { 4, () => P4.Run() },
+            { 5, () => P5.Run() },
         };
         #endregion
 
@@ -48,6 +51,12 @@ class P1
                 result += temp;
         }
 
+        Console.WriteLine("Result for sum under 1000: " + result);
+
+        Console.WriteLine("\n\nType maxima");
+        Int32.TryParse(Console.ReadLine(), out temp);
+
+        result = 3 * (int)(temp / 3) * (1 + (int)(temp / 3)) / 2 + 5 * (int)(temp / 5) * (1 + (int)(temp / 5)) / 2 - 15 * (int)(temp / 15) * (1 + (int)(temp / 15)) / 2;
         Console.WriteLine(result);
     }
 }
@@ -58,7 +67,56 @@ class P2
 {
     public static void Run()
     {
-        Console.Write("2");
+        int x = 1, y = 1, maxima = 0;
+        long result = 0;
+
+        while (true)
+        {
+            maxima = x + y;
+
+            if (maxima <= 4000000)
+            {
+                if (maxima % 2 == 0)
+                    result += maxima;
+            }
+            else
+                break;
+
+            x = y;
+            y = maxima;
+        }
+
+        Console.WriteLine(result);
+    }
+}
+#endregion
+
+#region #3
+class P3
+{
+    public static void Run()
+    {
+        Console.WriteLine("Empty");
+    }
+}
+#endregion
+
+#region #4
+class P4
+{
+    public static void Run()
+    {
+        Console.WriteLine("Empty");
+    }
+}
+#endregion
+
+#region #5
+class P5
+{
+    public static void Run()
+    {
+        Console.WriteLine("Empty");
     }
 }
 #endregion
