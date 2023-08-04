318d42f709e8fd3dd1639b34b8c12e51006129e9
Rh4ps0dy <bartosz.giera1219@gmail.com>
2023-08-04 23:22:24 +0200

    Added Problem no.8 solution

--- EulerProject/Program.cs
+++ EulerProject/Program.cs
@@ -33,7 +33,6 @@ class ProgramRunner
         Int32.TryParse(Console.ReadLine(), out problemNumber);
 
         Console.WriteLine($"\n\nStarting solution to problem no.{problemNumber}...\n");
-        Thread.Sleep(500);
 
         if (problemsMap.TryGetValue(problemNumber, out var x))
             x();
@@ -322,7 +321,32 @@ class P8
 {
     public static void Run()
     {
-        Console.WriteLine("Empty");
+        string input = "7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450";
+        string[] arr = input.Split('0');
+
+        long maxFound = 0;
+        long timer = Stopwatch.GetTimestamp();
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i].Length < 12)
+                continue;
+
+            for (int j = 0; j < arr[i].Length - 12; j++)
+            {
+                long temp = long.Parse(arr[i][j].ToString()) * long.Parse(arr[i][j + 1].ToString()) * long.Parse(arr[i][j + 2].ToString()) *
+                            long.Parse(arr[i][j + 3].ToString()) * long.Parse(arr[i][j + 4].ToString()) * long.Parse(arr[i][j + 5].ToString()) *
+                            long.Parse(arr[i][j + 6].ToString()) * long.Parse(arr[i][j + 7].ToString()) * long.Parse(arr[i][j + 8].ToString()) *
+                            long.Parse(arr[i][j + 9].ToString()) * long.Parse(arr[i][j + 10].ToString()) * long.Parse(arr[i][j + 11].ToString()) *
+                            long.Parse(arr[i][j + 12].ToString());
+
+                if (temp > maxFound)
+                    maxFound = temp;
+            }
+        }
+
+        Console.WriteLine($"Greatest product of thirteen adjacent digits = {maxFound}");
+        Console.WriteLine($"Calculated in {Stopwatch.GetElapsedTime(timer).ToString(@"mm\:ss\.fff")}");
     }
 }
 #endregion
